8309547814ac8b1705981743a5041aaf8b757d84
KarasawaTakahiro <tkhr.karasawa@gmail.com>
2020-01-06 22:52:47 +0900

    Model#save

--- model_sample.rb
+++ model_sample.rb
@@ -5,13 +5,25 @@
 
 
 # database
-users_table = []
+$users_table = []
 
 # User Model
 class User
+  def save
+    $users_table << self
+    puts "log: #{self} was saved!"
+  end
 end
+
 
 # Model.new
 user = User.new
 
-p user
+puts "user = #{user}"
+
+# Model#save
+puts "users_table = #{$users_table}"
+
+user.save
+
+puts "users_table = #{$users_table}"
