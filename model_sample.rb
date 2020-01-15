103a3449f46cd2577dd074b54a8d141a00e84696
KarasawaTakahiro <karasawa@basicinc.jp>
2020-01-15 17:21:20 +0900

    Model#save

--- model_sample.rb
+++ model_sample.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Model.new
 # Model#save
 # Model.create
@@ -8,8 +10,17 @@ $humans_table = []
 
 # User Model
 class HumanClass
+  def save
+    $humans_table << self
+    puts "log: #{self} was saved!"
+  end
 end
 
 # Model.new
 human_instance = HumanClass.new
 puts "human = #{human_instance}"
+
+# Model#save
+puts "humans_table = #{$humans_table}"
+human_instance.save
+puts "humans_table = #{$humans_table}"
